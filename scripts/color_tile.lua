48f23b5e0b274ee425a38efd19fea89f42f44fa6
Arthur <arthurcose@gmail.com>
2021-03-03 09:03:30 -0500

    Swap color_tile for map_building script

--- scripts/color_tile.lua
+++
@@ -1,8 +0,0 @@
-function handle_player_move(player_id, x, y, z)
-    if x < 0 or y < 0 then return end
-
-    local area_id = Net.get_player_area(player_id)
-
-    Net.set_tile(area_id, x, y, z, 1)
-end
-
