fbd38783ddacb6500e99e93bcf51ccda77d7ec04
Arthur <arthurcose@gmail.com>
2021-03-10 18:40:57 -0500

    Fix indentation in map_building script

--- scripts/map_building.lua
+++ scripts/map_building.lua
@@ -1,36 +1,35 @@
 function handle_tile_interaction(player_id, x, y, z)
-    if x < 0 or y < 0 then return end
+  if x < 0 or y < 0 then return end
 
-    x = math.floor(x)
-    y = math.floor(y)
-    z = math.floor(z)
+  x = math.floor(x)
+  y = math.floor(y)
+  z = math.floor(z)
 
-    local area_id = Net.get_player_area(player_id)
-    local tile_gid = Net.get_tile_gid(area_id, x, y, z)
+  local area_id = Net.get_player_area(player_id)
+  local tile_gid = Net.get_tile_gid(area_id, x, y, z)
 
-
-    if tile_gid == 0 then
-        Net.set_tile(area_id, x, y, z, 1)
-    elseif tile_gid == 1 and not has_player(area_id, x, y, z) then
-        Net.set_tile(area_id, x, y, z, 0)
-    end
+  if tile_gid == 0 then
+    Net.set_tile(area_id, x, y, z, 1)
+  elseif tile_gid == 1 and not has_player(area_id, x, y, z) then
+    Net.set_tile(area_id, x, y, z, 0)
+  end
 end
 
 function has_player(area_id, x, y, z)
-    local player_ids = Net.list_players(area_id)
+  local player_ids = Net.list_players(area_id)
 
-    for i = 1, #player_ids, 1 do
-      local player_pos = Net.get_player_position(player_ids[i])
+  for i = 1, #player_ids, 1 do
+    local player_pos = Net.get_player_position(player_ids[i])
 
-      if
-        x == math.floor(player_pos.x) and
-        y == math.floor(player_pos.y) and
-        z == math.floor(player_pos.z)
-      then
-        -- block updates to this tile
-        return true
-      end
+    if
+      x == math.floor(player_pos.x) and
+      y == math.floor(player_pos.y) and
+      z == math.floor(player_pos.z)
+    then
+      -- block updates to this tile
+      return true
     end
+  end
 
-    return false
+  return false
 end
