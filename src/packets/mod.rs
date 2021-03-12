0b14119b7f4426fdeaa3fb1d711d9e24023b528a
Arthur <arthurcose@gmail.com>
2021-03-12 04:02:09 -0500

    Remove unnecessary pub

--- src/packets/mod.rs
+++ src/packets/mod.rs
@@ -14,5 +14,5 @@ pub struct PacketHeaders {
   pub id: u64,
 }
 
-pub(super) const VERSION_ID: &str = "https://github.com/ArthurCose/Scriptable-OpenNetBattle-Server";
-pub(super) const VERSION_ITERATION: u64 = 1;
+const VERSION_ID: &str = "https://github.com/ArthurCose/Scriptable-OpenNetBattle-Server";
+const VERSION_ITERATION: u64 = 1;
