6e0de14d3f5d7ab734c2607f9597e1aa9f37bbd6
Arthur <arthurcose@gmail.com>
2021-03-31 07:08:45 -0400

    Update packet version

--- src/packets/mod.rs
+++ src/packets/mod.rs
@@ -14,6 +14,5 @@ pub struct PacketHeaders {
   pub id: u64,
 }
 
-const VERSION_ID: &str =
-  "https://github.com/ArthurCose/Scriptable-OpenNetBattle-Server/tree/development";
-const VERSION_ITERATION: u64 = 9;
+const VERSION_ID: &str = "https://github.com/ArthurCose/Scriptable-OpenNetBattle-Server";
+const VERSION_ITERATION: u64 = 5;
