f6a4b6a8f12449c737c7f63761af81d698e7b859
Arthur Cosentino <arthurcose@gmail.com>
2021-08-22 15:03:58 -0400

    Add emotes lua lib

---
+++ scripts/libs/emotes.lua
@@ -0,0 +1,25 @@
+local Emotes = {
+  HAPPY = 0,
+  BLANK = 1,
+  MAD = 2,
+  SAD = 3,
+  SMUG = 4,
+  SHOCKED = 5,
+  COB = 6,
+  PVP = 7,
+  ZZZ = 8,
+  KEY = 9,
+  QUESTION = 10,
+  ALERT = 11,
+  HEART = 12,
+  GG = 13,
+  LOL = 14,
+  YES = 15,
+  NO = 16,
+  VOMIT = 17,
+  COOL = 18,
+  UNCOOL = 19,
+  SPIRIT = 20,
+}
+
+return Emotes
