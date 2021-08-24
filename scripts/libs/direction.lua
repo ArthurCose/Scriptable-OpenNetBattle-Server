40f930562465e4d24369d14861877c09f0e2a564
Arthur Cosentino <arthurcose@gmail.com>
2021-08-24 17:37:37 -0400

    Add diagonal helper functions for Direction

--- scripts/libs/direction.lua
+++ scripts/libs/direction.lua
@@ -36,6 +36,18 @@ function Direction.from_points(point_a, point_b)
   return Direction.from_offset(b_x - a_x, b_y - a_y)
 end
 
+function Direction.diagonal_from_points(point_a, point_b)
+  local a_z_offset = point_a.z / 2
+  local a_x = point_a.x - a_z_offset
+  local a_y = point_a.y - a_z_offset
+
+  local b_z_offset = point_b.z / 2
+  local b_x = point_b.x - b_z_offset
+  local b_y = point_b.y - b_z_offset
+
+  return Direction.diagonal_from_offset(b_x - a_x, b_y - a_y)
+end
+
 local directions = {
   "Up Left",
   "Up",
@@ -51,6 +63,24 @@ function Direction.from_offset(x, y)
   local angle = math.atan(y, x)
   local direction_index = math.floor(angle / math.pi * 4) + 5
   return directions[direction_index]
+end
+
+function Direction.diagonal_from_offset(x, y)
+  if math.abs(x) > math.abs(y) then
+    -- x axis direction
+    if x < 0 then
+      return Direction.UP_LEFT
+    else
+      return Direction.DOWN_RIGHT
+    end
+  else
+    -- y axis direction
+    if y < 0 then
+      return Direction.UP_RIGHT
+    else
+      return Direction.DOWN_LEFT
+    end
+  end
 end
 
 return Direction
