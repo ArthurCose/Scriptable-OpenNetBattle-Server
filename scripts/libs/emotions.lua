9b82886e439bcb06d24014fcea5ce701ecd6bff8
Arthur Cosentino <arthurcose@gmail.com>
2021-10-29 18:25:42 -0400

    Add emotions lib

---
+++ scripts/libs/emotions.lua
@@ -0,0 +1,14 @@
+local Emotions = {
+  NORMAL = 0,
+  FULL_SYNCHRO = 1,
+  ANGRY = 2,
+  EVIL = 3,
+  ANXIOUS = 4,
+  TIRED = 5,
+  EXHAUSTED = 6,
+  PINCH = 7,
+  FOCUS = 8,
+  HAPPY = 9,
+}
+
+return Emotions
